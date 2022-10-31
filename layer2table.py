29ee22cb40ee3368c0f2eea918be867279f1f7c5
tuccitj <71199179+tuccitj@users.noreply.github.com>
2022-10-31 01:05:49 -0400

    cleaned up code

--- layer2table.py
+++ layer2table.py
@@ -1,24 +1,28 @@
 import requests
 import logging
 import json
-from pprint import pprint
+import os
 
 
 class GisLayer2Table(object):
-    def __init__(self, parent_logger=None, conversion_table_source=None) -> None:
+    def __init__(self, output_path, parent_logger=None, conversion_table_source=None):
+
+        self.output_path = output_path
+        # Supports logging. Pass a parent logger or one will be created
         if parent_logger:
             self._logger = parent_logger.getChild('gislayer2table')
         else:
             self._logger = logging.getLogger('gislayer2table')
-            
+
         if conversion_table_source:
             self.conversion_table_source = conversion_table_source
         else:
             self._logger.error("No conversion table source provided")
-        # Supports logging. Pass a parent logger or one will be created
-       
 
-    def _get_metadata(self, feature_layer_url):
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
+
+    def _getMetadata(self, feature_layer_url):
         try:
             response = requests.get(feature_layer_url + "/?f=json")
             if response.status_code != 200:
@@ -32,7 +36,7 @@ class GisLayer2Table(object):
         except Exception as e:
             self._logger.error(e)
 
-    def _get_fields(self, metadata):
+    def _getFields(self, metadata):
         fields = ''
         try:
             fields = metadata["fields"]
@@ -41,32 +45,44 @@ class GisLayer2Table(object):
                 "Unable to find fields in dict, check your input object")
         return fields
 
-    def _parse_fields(self, field):
+    def _parseFields(self, field):
         result = dict(field_name=None, field_type=None)
         result['field_name'] = field['name']
         result['field_type'] = field['type']
         return result
-        
-    def _convertFields(self, field, output_type):
-        
-        with open(self.conversion_table_source, 'r') as infile:
-            conversion_dict = json.load(infile)
 
-        if output_type == 'tsql':
-            try:
-                field['field_type'] = conversion_dict[field['field_type']]
-                return field
-            except:
-                self._logger.debug(f"No field conversion found for {field}.")
-                return f"Unable to convert {field}"
-        
-    def generateScript(self, url, output_type, output_path) -> None:
-        result = ''
-        metadata = self._get_metadata(url)
-        fields = self._get_fields(metadata)
+    def _convertFields(self, field, conversion_dict):
+        try:
+            field['field_type'] = conversion_dict[field['field_type']]
+            return field
+        except:
+            self._logger.debug(f"No field conversion found for {field}.")
+            return f"Unable to convert {field}"
+    def _writeScriptToFile(self, converted_fields):
+       
+        with open(self.output_path + "create_" + self.layer_name + ".sql", "w") as f:
+                    f.write(f"CREATE TABLE {self.layer_name} (")
+                    count = 0
+                    for cf in converted_fields:
+                        f.write(f"{cf['field_name']} {cf['field_type']}")
+                        if count < len(converted_fields)-1:
+                            f.write(',')
+                        count = count + 1
+                    f.write(");")
+                    
+    def generateScript(self, url):
+
+        metadata = self._getMetadata(url)
+        self.layer_name = metadata['name'].replace(' ', '')
+        fields = self._getFields(metadata)
         if fields:
-            parsed_fields = [self._parse_fields(field) for field in fields]
-            converted_fields = [self._convertFields(parsed_field, output_type) for parsed_field in parsed_fields]
-        return result
-    
-        
+            parsed_fields = [self._parseFields(field) for field in fields]
+            
+            with open(self.conversion_table_source, 'r') as infile:
+                conversion_dict = json.load(infile)
+                
+                converted_fields = [self._convertFields(
+                    parsed_field, conversion_dict) for parsed_field in parsed_fields]
+            # clean this up
+            if converted_fields:
+                self._writeScriptToFile(converted_fields)
--- program.py
+++ program.py
@@ -1,23 +1,29 @@
 import logging
 from layer2table import GisLayer2Table
+from pprint import pprint
 
 
 def main():
-    
+    # initialize and configure logger
     log_format = '%(asctime)s|%(name)s|%(levelname)s|%(message)s'
     logging.basicConfig(filename='app.log', format=log_format,
                         encoding='utf-8', level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     logger.info("Logger Created")
 
+    # set path for generated script output
+    output_path = "generated-scripts/"
+
+    g2t = GisLayer2Table(output_path=output_path, parent_logger=logger,
+                         conversion_table_source="data\EsriPostgresTypeConversion.json")
+
     urls = ["https://maps.cityofrochester.gov/server/rest/services/App_PropertyInformation/App_Tax_Parcel_Viewer/FeatureServer/0",
-            "https://maps.cityofrochester.gov/server/rest/services/App_PropertyInformation/App_Tax_Parcel_Viewer/FeatureServer/011as"]
+            "https://maps.cityofrochester.gov/server/rest/services/App_PropertyInformation/App_Tax_Parcel_Viewer/FeatureServer/2",
+            "https://maps.cityofrochester.gov/server/rest/services/App_PropertyInformation/App_Tax_Parcel_Viewer/FeatureServer/9",
+            "https://maps.cityofrochester.gov/server/rest/services/App_PropertyInformation/App_Tax_Parcel_Viewer/FeatureServer/11"]
 
-    output_path = "./scripts/generated"
-
-    layer2table = GisLayer2Table(parent_logger=logger, conversion_table_source="data\EsriPostgresTypeConversion.json")
-    
     for url in urls:
-        result = layer2table.generateScript(url, output_type='tsql', output_path=output_path)
-        print(result)
+        result = g2t.generateScript(url)
+
+
 main()
